52f0e463d46ecfc71c0f33a465737af830fe19da
miebrahim <miebrahim@elm.sa>
2025-03-31 05:16:34 +0300

    Lect-43-Update-Login-Mapper-with-Extensions-for-Better-Code-Reusability

--- features/auth/login/lib/data/mapper/login_mapper.dart
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -1,9 +1,13 @@
+import 'package:extensions/constant/constant.dart';
+import 'package:extensions/extensions.dart';
 import 'package:login/data/response/login_response.dart';
 import 'package:login/domain/model/login_model.dart';
 
 extension LoginResponseMapper on LoginResponse? {
   LoginModel toDomain() {
     return LoginModel(
-        name: this?.name, phone: this?.phone, age: this?.age);
+        name: this?.name.orEmpty() ?? Constants.empty,
+        phone: this?.phone.orEmpty() ?? Constants.empty,
+        age: this?.age.orZero() ?? Constants.zero);
   }
 }
