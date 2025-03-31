b9208b51c27e832e0c24e82f7bd3fffbf9e3d2a5
miebrahim <miebrahim@elm.sa>
2025-03-31 05:05:53 +0300

    Lect-42-Adding-Extensions-Module-in-the-Core-Layer

--- features/auth/login/lib/data/mapper/login_mapper.dart
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -3,6 +3,7 @@ import 'package:login/domain/model/login_model.dart';
 
 extension LoginResponseMapper on LoginResponse? {
   LoginModel toDomain() {
-    return LoginModel(name: this?.name, phone: this?.phone, age: this?.age);
+    return LoginModel(
+        name: this?.name, phone: this?.phone, age: this?.age);
   }
 }
