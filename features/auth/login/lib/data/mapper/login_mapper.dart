fc445712554fee46e257e859b04270fcd996e2ea
miebrahim <miebrahim@elm.sa>
2025-03-31 04:16:57 +0300

    Lect-41-Using-Extension-Functions-for-Response-Mapping

--- features/auth/login/lib/data/mapper/login_mapper.dart
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -1,10 +1,8 @@
 import 'package:login/data/response/login_response.dart';
 import 'package:login/domain/model/login_model.dart';
 
-extension LoginResponseMapper on LoginResponse?{
-
+extension LoginResponseMapper on LoginResponse? {
   LoginModel toDomain() {
-    return LoginModel(name: this?.name, phone: this?.phone, age: this ? age);
+    return LoginModel(name: this?.name, phone: this?.phone, age: this?.age);
   }
-
 }
