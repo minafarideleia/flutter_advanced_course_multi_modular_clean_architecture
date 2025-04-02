57f249114b099ef8154d8beebaf6d52346203ac1
miebrahim <miebrahim@elm.sa>
2025-04-02 04:10:54 +0300

    Lect-48-Enhancing-Safe-API-Call-with-Network-Connectivity-Check

--- core/data/lib/network/api_safe_call.dart
+++ core/data/lib/network/api_safe_call.dart
@@ -1,13 +1,23 @@
 import 'package:dartz/dartz.dart';
+import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:domain/model/localised_message.dart';
 
-Future<Either<Failure, T>> safeAPiCall<T>(Future<T> Function() apiCall) async {
-  try {
-    final response = await apiCall();
-    return Right(response);
-  } catch (error) {
-    return Left(
-        Failure(0, LocalisedMessage("", ""))); // todo add error handler here
+Future<Either<Failure, T>> safeAPiCall<T>(
+    NetworkInfo networkInfo, Future<T> Function() apiCall) async {
+  if (await networkInfo.isConnected) {
+    try {
+      final response = await apiCall();
+      return Right(response);
+    } catch (error) {
+      return Left(
+          Failure(0, LocalisedMessage("", ""))); // todo add error handler here
+    }
+  } else {
+    // no internet connection
+    return Left(Failure(
+        -500,
+        LocalisedMessage("network connectivity issue, please check",
+            ""))); // todo add error handler here
   }
 }
