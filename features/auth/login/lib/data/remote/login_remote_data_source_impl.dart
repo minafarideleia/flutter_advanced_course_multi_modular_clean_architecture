d6f89edc56076bcd6d57c46ec1d982a1ce2084b7
miebrahim <miebrahim@elm.sa>
2025-04-02 03:30:28 +0300

    Lect-46-Updating-Login-Repository-with-Safe-API-Call-and-Error-Handling

--- features/auth/login/lib/data/remote/login_remote_data_source_impl.dart
+++ features/auth/login/lib/data/remote/login_remote_data_source_impl.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:data/network/api_safe_call.dart';
+import 'package:domain/model/failure.dart';
 import 'package:login/data/remote/login_remote_data_source.dart';
 import 'package:login/data/request/login_request.dart';
 import 'package:login/data/response/login_response.dart';
 import 'package:login/data/service/login_service.dart';
-import 'package:retrofit/dio.dart';
 
 class LoginRemoteDataSourceImpl implements LoginRemoteDataSource {
   final LoginService loginService;
@@ -10,7 +12,13 @@ class LoginRemoteDataSourceImpl implements LoginRemoteDataSource {
   LoginRemoteDataSourceImpl(this.loginService);
 
   @override
-  Future<HttpResponse<LoginResponse>> login(LoginRequest loginRequest) async {
-    return await loginService.login(loginRequest.email, loginRequest.password);
+  Future<Either<Failure, LoginResponse>> login(
+      LoginRequest loginRequest) async {
+    return safeAPiCall(() async {
+      final response =
+          await loginService.login(loginRequest.email, loginRequest.password);
+
+      return response.data;
+    });
   }
 }
