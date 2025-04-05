a709aef4484660e6050bd3f5b9d78bc3e158d696
miebrahim <miebrahim@elm.sa>
2025-04-05 04:22:39 +0300

    Lect-63-Update-Data-DI-Module-with-Session-and-Preferences-Providers

--- core/data/lib/di/data_module.dart
+++ core/data/lib/di/data_module.dart
@@ -1,36 +1,47 @@
 import 'package:data/factory/dio_factory.dart';
+import 'package:datastore/provider/preferences/preferences_provider.dart';
+import 'package:datastore/provider/preferences/preferences_provider_impl.dart';
+import 'package:datastore/provider/session/session_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'data_module_keys.dart';
 
 @module
 abstract class DataModule {
   @preResolve
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 
+  @lazySingleton
+  PreferencesProvider providePreferencesProviderImpl(SharedPreferences prefs) =>
+      PreferencesProviderImpl(prefs);
+
+  // todo check me if still need it
+  // @lazySingleton
+  // SessionProvider provideSessionProviderImpl(SharedPreferences prefs) =>
+  //     SessionProviderImpl(prefs);
+
   // provide base url
-  @Named("BaseUrl") // a tag for this string
-  String get baseUrl => "base_url";
+  @Named(DataModuleKeys.baseUrl) // a tag for this string
+  String provideBaseUrl(PreferencesProvider preferencesProvider) =>
+      preferencesProvider.getBaseUrl();
 
-  @Named("AccessToken") // a tag for this string
-  Future<String> get accessToken async {
-    // return access token
-  }
+  @Named(DataModuleKeys.accessToken) // a tag for this string
+  String provideAccessToken(SessionProvider sessionProvider) =>
+      sessionProvider.getAccessToken();
 
-  @Named("Language") // a tag for this string
-  Future<String> get language async {
-    // return language
-  }
+  @Named(DataModuleKeys.language) // a tag for this string
+  String provideLanguage(PreferencesProvider preferencesProvider) =>
+      preferencesProvider.getAppLanguage();
 
   @lazySingleton
   Future<Dio> dio(
-      @Named("BaseUrl") String baseUrl,
-      @Named("AccessToken") Future<String> accessToken,
-      @Named("Language") Future<String> language) async {
+      @Named(DataModuleKeys.baseUrl) String baseUrl,
+      @Named(DataModuleKeys.accessToken) String accessToken,
+      @Named(DataModuleKeys.language) String language) async {
     final dioFactory = DioFactory(
-        baseUrl: baseUrl,
-        accessToken: await accessToken,
-        language: await language);
+        baseUrl: baseUrl, accessToken: accessToken, language: language);
 
     return dioFactory.getDio();
   }
