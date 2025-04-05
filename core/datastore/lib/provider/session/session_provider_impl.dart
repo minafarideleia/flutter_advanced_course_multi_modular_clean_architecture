70a0cfd68a56b5796f0d3c5c33563a1d6c61f0da
miebrahim <miebrahim@elm.sa>
2025-04-05 03:58:23 +0300

    Lect-62-Implement-Session-Provider-with-SharedPreferences

--- core/datastore/lib/provider/session/session_provider_impl.dart
+++ core/datastore/lib/provider/session/session_provider_impl.dart
@@ -1,51 +1,90 @@
+import 'package:datastore/provider/session/session_strings.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 import 'session_provider.dart';
 
 @Injectable(as: SessionProvider, env: [Environment.prod])
 class ProdSessionProviderImpl extends SessionProvider {
+  final SharedPreferences _sharedPreferences;
+
+  ProdSessionProviderImpl(this._sharedPreferences);
+
   @override
   String getAccessToken() {
-    return "access token from user session API";
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setAccessToken(String accessToken) {
+    _sharedPreferences.setString(SessionStrings.accessTokenKey, accessToken);
   }
 
   @override
   String getClientId() {
-    return "111111111";
+    return Uuid().v4();
   }
 
   @override
   String getRefreshToken() {
-    return "refresh token from user session API";
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setRefreshToken(String refreshToken) {
+    _sharedPreferences.setString(SessionStrings.refreshTokenKey, refreshToken);
   }
 
   @override
   String getUserId() {
-    return "123456789";
+    return _sharedPreferences.getString(SessionStrings.userIdKey) ?? "";
+  }
+
+  @override
+  void setUserId(String userId) {
+    _sharedPreferences.setString(SessionStrings.userIdKey, userId);
   }
 }
-
 
 @Injectable(as: SessionProvider, env: [Environment.dev])
 class DevSessionProviderImpl extends SessionProvider {
+  final SharedPreferences _sharedPreferences;
+
+  DevSessionProviderImpl(this._sharedPreferences);
+
   @override
   String getAccessToken() {
-    return "access token from user session API";
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setAccessToken(String accessToken) {
+    _sharedPreferences.setString(SessionStrings.accessTokenKey, accessToken);
   }
 
   @override
   String getClientId() {
-    return "222222222222";
+    return Uuid().v4();
   }
 
   @override
   String getRefreshToken() {
-    return "refresh token from user session API";
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setRefreshToken(String refreshToken) {
+    _sharedPreferences.setString(SessionStrings.refreshTokenKey, refreshToken);
   }
 
   @override
   String getUserId() {
-    return "123456789";
+    return _sharedPreferences.getString(SessionStrings.userIdKey) ?? "";
+  }
+
+  @override
+  void setUserId(String userId) {
+    _sharedPreferences.setString(SessionStrings.userIdKey, userId);
   }
 }
-
