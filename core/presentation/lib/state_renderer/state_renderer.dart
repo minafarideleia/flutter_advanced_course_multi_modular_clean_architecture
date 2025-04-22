bfdc0329e3a9434b9ce156c5c3f6318db1080f4c
miebrahim <miebrahim@elm.sa>
2025-04-23 00:04:58 +0300

    Lect-91-Handling-Full-Screen-Empty-State

--- core/presentation/lib/state_renderer/state_renderer.dart
+++ core/presentation/lib/state_renderer/state_renderer.dart
@@ -10,12 +10,11 @@ class StateRenderer extends StatelessWidget {
   var _isDialogShowing = false;
   final VoidCallback? retryActionFunction;
 
-  StateRenderer(
-      {super.key,
-      required this.stateRendererType,
-      this.message = "Loading...",
-      this.title = "Error",
-      this.retryActionFunction});
+  StateRenderer({super.key,
+    required this.stateRendererType,
+    this.message = "Loading...",
+    this.title = "Error",
+    this.retryActionFunction});
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +28,21 @@ class StateRenderer extends StatelessWidget {
       case StateRendererType.fullScreenErrorState:
         return _showFullScreenContent(_buildErrorWidget(showRetryButton: true));
       case StateRendererType.emptyState:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _showFullScreenContent(_buildEmptyWidget());
       case StateRendererType.contentState:
-        // TODO: Handle this case.
+      // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.none:
-        // TODO: Handle this case.
+      // TODO: Handle this case.
         throw UnimplementedError();
     }
   }
 
   // it will check if we are displaying a dialog.
   _isThereCurrentDialogShowing(BuildContext context) =>
-      ModalRoute.of(context)?.isCurrent != true;
+      ModalRoute
+          .of(context)
+          ?.isCurrent != true;
 
   Widget _buildLoadingWidget() {
     return Column(
@@ -59,13 +59,28 @@ class StateRenderer extends StatelessWidget {
     if (!_isThereCurrentDialogShowing(context)) {
       _isDialogShowing = true;
 
-      WidgetsBinding.instance.addPersistentFrameCallback((_) => showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                content: content,
-              )));
+      WidgetsBinding.instance.addPersistentFrameCallback((_) =>
+          showDialog(
+              context: context,
+              builder: (context) =>
+                  AlertDialog(
+                    content: content,
+                  )));
     }
     return Container(); // keep screen content behind the dialog
+  }
+
+  Widget _buildEmptyWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.inbox,
+          size: 50,
+          color: Colors.grey,),
+        SizedBox(height: 10,),
+        Text("No Data Available")
+      ],
+    );
   }
 
   Widget _buildErrorWidget({bool showRetryButton = false}) {
@@ -98,20 +113,22 @@ class StateRenderer extends StatelessWidget {
     if (!_isThereCurrentDialogShowing(context)) {
       _isDialogShowing = true;
 
-      WidgetsBinding.instance.addPersistentFrameCallback((_) => showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                content: content,
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        _isDialogDismissed = true;
-                        Navigator.of(context, rootNavigator: true)
-                            .pop(true); // dismiss the dialog
-                      },
-                      child: Text("Close"))
-                ],
-              )));
+      WidgetsBinding.instance.addPersistentFrameCallback((_) =>
+          showDialog(
+              context: context,
+              builder: (context) =>
+                  AlertDialog(
+                    content: content,
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            _isDialogDismissed = true;
+                            Navigator.of(context, rootNavigator: true)
+                                .pop(true); // dismiss the dialog
+                          },
+                          child: Text("Close"))
+                    ],
+                  )));
     }
     return Container(); // keep screen content behind the dialog
   }
