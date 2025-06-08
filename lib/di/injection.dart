e770a37efa26b10f46d25b015ac26efb680fbf1a
miebrahim <miebrahim@elm.sa>
2025-06-08 13:41:38 +0300

    Lect-130-Creating-Cached-Movie-Model

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -3,6 +3,8 @@ import 'package:data/di/injection.dart';
 import 'package:datastore/di/injection.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_advanced_course_multi_modular_clean_architecture/di/injection.config.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:injectable/injectable.dart';
 import 'package:login/data/di/injection.dart';
 import 'package:movies/data/di/injection.dart';
@@ -11,6 +13,7 @@ final getIt = GetIt.instance;
 
 @InjectableInit()
 Future<void> configureDependencies(String? environment) async {
+  Hive.initFlutter();
   await configureAppSettingsDependencies(getIt, environment);
   await configureDataStoreDependencies(getIt, environment);
   await configureCoreDataDependencies(getIt, environment);
